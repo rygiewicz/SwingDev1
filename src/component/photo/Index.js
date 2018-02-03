4aa562a4653820bc5f8f7fdc306a03f3f9410448
prygiewicz <pawel.rygiewicz@kalicinscy.com>
2018-02-03 19:59:46 +0100

    Change photo/Index container to be based on Component

--- src/component/photo/Index.js
+++ src/component/photo/Index.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Container} from 'flux/utils';
 
 import PhotoList from './PhotoList';
 
 import PhotoStore from "../../data/photo/PhotoStore";
 
-function getStores() {
-    return [
-        PhotoStore,
-    ];
+class PhotoIndex extends Component {
+    static getStores() {
+        return [
+            PhotoStore,
+        ];
+    }
+
+    static calculateState() {
+        return {
+            photos: PhotoStore.getState(),
+        };
+    }
+
+    render() {
+        return (
+            <div id="photo-index">
+                <PhotoList photos={this.state.photos}/>
+            </div>
+        );
+    }
 }
 
-function getState() {
-    return {
-        photos: PhotoStore.getState(),
-    };
-}
-
-export default Container.createFunctional(PhotoList, getStores, getState);
+export default Container.create(PhotoIndex);
