0159a54f4889107597f7fc1f0b2af7ca5cb8fc3d
prygiewicz <pawel.rygiewicz@kalicinscy.com>
2018-02-03 19:43:57 +0100

    Move photos mock to PhotoStore

--- src/component/photo/Index.js
+++ src/component/photo/Index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import PhotoRecord from "../../domain/photo/Photo";
-import Immutable from "immutable";
 import {Container} from 'flux/utils';
 
 import PhotoList from './PhotoList';
@@ -15,28 +13,8 @@ function getStores() {
 
 function getState() {
     return {
-        photos: mockPhotos(),
+        photos: PhotoStore.getState(),
     };
-}
-
-function mockPhotos() {
-    return Immutable.List([
-        new PhotoRecord({
-            author: 'photo1',
-            date: new Date('2001-12-10'),
-            description: 'photo1 description'
-        }),
-        new PhotoRecord({
-            author: 'photo2',
-            date: new Date('2008-05-17'),
-            description: 'photo2 description'
-        }),
-        new PhotoRecord({
-            author: 'photo3',
-            date: new Date('1997-02-23'),
-            description: 'photo3 description'
-        })
-    ]);
 }
 
 export default Container.createFunctional(PhotoList, getStores, getState);
--- src/data/photo/PhotoStore.js
+++ src/data/photo/PhotoStore.js
@@ -1,6 +1,7 @@
 import {ReduceStore} from 'flux/utils';
 import PhotoDispatcher from './PhotoDispatcher'
 import Immutable from 'immutable';
+import PhotoRecord from "../../domain/photo/Photo";
 
 class PhotoStore extends ReduceStore {
     constructor() {
@@ -8,7 +9,23 @@ class PhotoStore extends ReduceStore {
     }
 
     getInitialState() {
-        return Immutable.List();
+        return Immutable.List([
+            new PhotoRecord({
+                author: 'photo1',
+                date: new Date('2001-12-10'),
+                description: 'photo1 description'
+            }),
+            new PhotoRecord({
+                author: 'photo2',
+                date: new Date('2008-05-17'),
+                description: 'photo2 description'
+            }),
+            new PhotoRecord({
+                author: 'photo3',
+                date: new Date('1997-02-23'),
+                description: 'photo3 description'
+            })
+        ]);
     }
 }
 
